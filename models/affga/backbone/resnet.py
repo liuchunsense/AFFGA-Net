2b46712583f43648d2dbbe3552864ce965d53ffe
liuchunsense <liuchunsheng@sdu.edu.cn>
2021-10-07 18:10:14 +0800

    Update resnet.py

--- models/affga/backbone/resnet.py
+++ models/affga/backbone/resnet.py
@@ -147,8 +147,7 @@ class ResNet(nn.Module):
                 m.bias.data.zero_()
 
     def _load_pretrained_model(self):
-        # pretrain_dict = model_zoo.load_url('https://download.pytorch.org/models/resnet101-5d3b4d8f.pth')
-        pretrain_dict = torch.load('/home/wangdx/research/affga_net/NN/resnet101-5d3b4d8f.pth')
+        pretrain_dict = model_zoo.load_url('https://download.pytorch.org/models/resnet101-5d3b4d8f.pth')
         model_dict = {}
         state_dict = self.state_dict()
         for k, v in pretrain_dict.items():
